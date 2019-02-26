b881f82d038a5976fa2071fd18394b507d0d07d2
y.okudera <>
2019-02-27 01:46:19 +0900

    first commit.

---
+++ iOSCollectionViewDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCollectionViewDemoApp
+//
+//  Created by YukiOkudera on 2019/02/27.
+//  Copyright © 2019 Yuki Okudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
